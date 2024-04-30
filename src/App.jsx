cec17e5e95f1381a0e842b81f845f449c54df9e7
Timo-Turtiainen <timo.turtiainen@icloud.com>
2024-04-30 14:11:10 +0300

    added todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,108 @@
 import './App.css'
-import { useState, useRef } from 'react'
-import { Button, TextField, InputLabel } from '@mui/material'
-function App() {
-  const todoRef = useRef('')
-  const [items, setItems] = useState([])
+import { useState } from 'react'
+import {
+  Button,
+  TextField,
+  InputLabel,
+  Checkbox,
+  MenuItem,
+  Select,
+} from '@mui/material'
+import { v4 as uuidv4 } from 'uuid'
 
+function App() {
+  const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  // const [isComplete, setIsComplete] = useState(false)
+
+  // const sortedTodos = todos.map()
   function handleSubmit(e) {
     e.preventDefault()
-    const todo = todoRef.current
-    setItems()
-    console.log(todo)
+    if (inputValue.trim() !== '') {
+      setTodos([
+        ...todos,
+        {
+          id: uuidv4(),
+          title: inputValue,
+          complete: false,
+          priority: 'normal',
+        },
+      ])
+    }
+    clearInputs()
   }
+
+  function clearInputs() {
+    setInputValue('')
+  }
+
+  function handleCheckbox(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          }
+        } else {
+          return todo
+        }
+      })
+    )
+  }
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <h1>THE TODO OO</h1>
+      <form
+        onSubmit={e => handleSubmit(e)}
+        style={{ display: 'flex', flexDirection: 'row', marginBottom: '40px' }}>
         <TextField
           label='Lisää tehtävä'
           fullWidth
-          ref={todoRef}
-          onChange={({ target }) => target.value}
+          value={inputValue}
+          onChange={({ target }) => setInputValue(target.value)}
         />
+        <Button
+          variant='contained'
+          style={{ borderRadius: '0 20px 20px 0' }}
+          type='submit'>
+          Lisää
+        </Button>
       </form>
-      <Button>Lisää</Button>
-      {items.map((todo, index) => (
-        <InputLabel key={index}>{todo}</InputLabel>
+      <Select label='Prioriteetti'>
+        <MenuItem value='low'>Low</MenuItem>
+        <MenuItem value='normal'>Normal</MenuItem>
+        <MenuItem value='high'>High</MenuItem>
+      </Select>
+
+      {todos.map(todo => (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            margin: '20px',
+            borderStyle: 'dotted',
+            borderRadius: '10px',
+          }}
+          key={todo.id}>
+          <Checkbox
+            checked={todo.complete}
+            onChange={e => handleCheckbox(todo.id)}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
+          <InputLabel
+            key={todo.id}
+            style={
+              todo.complete
+                ? { textDecoration: 'line-through' }
+                : { textDecoration: 'none' }
+            }>
+            {todo.title}
+          </InputLabel>
+        </div>
       ))}
     </>
   )
