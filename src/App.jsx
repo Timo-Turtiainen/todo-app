e1cf844c57235444ac492ddd33c65375d5be82bf
Timo-Turtiainen <timo.turtiainen@icloud.com>
2024-05-02 15:15:28 +0300

    added priority select box

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import {
   Checkbox,
   MenuItem,
   Select,
+  FormControl,
 } from '@mui/material'
 import { v4 as uuidv4 } from 'uuid'
+import { FaRegTrashAlt } from 'react-icons/fa'
 
 function App() {
   const [todos, setTodos] = useState([])
   const [inputValue, setInputValue] = useState('')
+  const [priority, setPriority] = useState('')
 
   // const [isComplete, setIsComplete] = useState(false)
 
@@ -24,9 +27,10 @@ function App() {
         ...todos,
         {
           id: uuidv4(),
-          title: inputValue,
+          task: inputValue,
           complete: false,
-          priority: 'normal',
+          priority: priority,
+          hoverered: false,
         },
       ])
     }
@@ -51,9 +55,37 @@ function App() {
       })
     )
   }
+  function handleMouseLeave(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            hoverered: !todo.hoverered,
+          }
+        } else {
+          return todo
+        }
+      })
+    )
+  }
+  // AA6AAA
+  // EAE4EA
+
+  function handleDelete(id) {
+    const todo = todos.find(todo => todo.id === id)
+
+    if (window.confirm(`Haluatko todella poistaa ${todo.task} tehtävän?`)) {
+      setTodos(todos.filter(todo => todo.id !== id))
+    }
+  }
+
+  function handlePriorityChange(e) {
+    setPriority(e.target.value)
+  }
 
   return (
-    <>
+    <div style={{}}>
       <h1>THE TODO OO</h1>
       <form
         onSubmit={e => handleSubmit(e)}
@@ -64,6 +96,21 @@ function App() {
           value={inputValue}
           onChange={({ target }) => setInputValue(target.value)}
         />
+
+        <FormControl sx={{ minWidth: 120 }}>
+          <InputLabel id='priority-label'>Prioriteetti</InputLabel>
+          <Select
+            labelId='priority-label'
+            id='priority-select'
+            value={priority}
+            onChange={handlePriorityChange}
+            autoWidth
+            label='Prioriteetti'>
+            <MenuItem value='Matala'>Matala</MenuItem>
+            <MenuItem value='Normaali'>Normaali</MenuItem>
+            <MenuItem value='Korkea'>Korkea</MenuItem>
+          </Select>
+        </FormControl>
         <Button
           variant='contained'
           style={{ borderRadius: '0 20px 20px 0' }}
@@ -71,40 +118,54 @@ function App() {
           Lisää
         </Button>
       </form>
-      <Select label='Prioriteetti'>
-        <MenuItem value='low'>Low</MenuItem>
-        <MenuItem value='normal'>Normal</MenuItem>
-        <MenuItem value='high'>High</MenuItem>
-      </Select>
-
-      {todos.map(todo => (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            margin: '20px',
-            borderStyle: 'dotted',
-            borderRadius: '10px',
-          }}
-          key={todo.id}>
-          <Checkbox
-            checked={todo.complete}
-            onChange={e => handleCheckbox(todo.id)}
-            inputProps={{ 'aria-label': 'controlled' }}
-          />
-          <InputLabel
-            key={todo.id}
-            style={
-              todo.complete
-                ? { textDecoration: 'line-through' }
-                : { textDecoration: 'none' }
-            }>
-            {todo.title}
-          </InputLabel>
-        </div>
-      ))}
-    </>
+      <div style={{}}>
+        {todos.map(todo => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              margin: '20px',
+              borderStyle: 'dotted',
+              borderRadius: '10px',
+              padding: '0px',
+              justifyContent: 'space-between',
+              backgroundColor: '#ffff',
+            }}
+            key={todo.id}>
+            <Checkbox
+              checked={todo.complete}
+              onChange={e => handleCheckbox(todo.id)}
+              inputProps={{ 'aria-label': 'controlled' }}
+            />
+            <InputLabel
+              key={todo.id}
+              style={
+                todo.complete
+                  ? { textDecoration: 'line-through' }
+                  : { textDecoration: 'none' }
+              }>
+              {todo.task}
+            </InputLabel>
+            {todo.hoverered ? (
+              <Button
+                style={{ height: '100%' }}
+                variant='contained'
+                onClick={() => handleDelete(todo.id)}
+                onMouseLeave={() => handleMouseLeave(todo.id)}>
+                Delete
+              </Button>
+            ) : (
+              <FaRegTrashAlt
+                size={21}
+                onMouseEnter={() => handleMouseLeave(todo.id)}
+                style={{ paddingRight: '20px' }}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
